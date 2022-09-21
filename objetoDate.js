44d5bb6341d558b6a4ebe6731f047b61e7f12ddd
feelsaron <pedrofreitas1128@gmail.com>
2022-09-21 18:15:35 -0300

    cria arquivo objetoDate

---
+++ objetoDate.js
@@ -0,0 +1,11 @@
+// para retornar data e hora atual 
+let agora = new Date()
+
+// para alterar data 
+let passado = new Date(2006, 1, 17)
+console.log(passado.toLocaleDateString())
+
+// data mais clara de entender 
+console.log(agora.toLocaleDateString("pt-BR"))
+
+
